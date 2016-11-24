cf541cdc83d91b08c0de123d8097b043b2dc7c90
Ian Russell <ian.andrew.russell@gmail.com>
2016-11-24 16:12:11 -0500

    create new files and base layout for game

---
+++ ttt.js
@@ -0,0 +1,12 @@
+$(function() {
+    var turn = 0
+
+    $('td').on('click', function(){
+    if (turn % 2 === 0) {
+        $(this).html('X');
+    } else {
+      $(this).html('O');
+    }
+  turn++;
+  });
+});
