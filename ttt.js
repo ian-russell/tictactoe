d2aefd1c54177cdda6aa3154447040cce6ab7367
Ian Russell <ian.andrew.russell@gmail.com>
2016-11-28 14:59:12 -0500

    parsing integer for id attribute for table cell. Win conditions bug needs fixing

--- ttt.js
+++ ttt.js
@@ -1,12 +1,42 @@
-$(function() {
-    var turn = 0
 
-    $('td').on('click', function(){
-    if (turn % 2 === 0) {
-        $(this).html('X');
-    } else {
+
+
+$(function() {
+
+  var turn = 0;
+  var bob = 0
+  var sally = 0
+
+
+  $('td').on('click', function() {
+     if ( turn % 2 === 0) {
+      $(this).html('X');
+      bob = parseInt($(this).attr('id')) + bob;
+      checkWin('X');
+      turn++;
+
+    } else  {
       $(this).html('O');
+       sally = parseInt($(this).attr('id')) + sally;
+       checkWin('O');
+       turn++;
+}
+
+//Checking win conditions of players
+//This needs modifying - not complete.  Player win won't be recognized if 4 moves have been played. Bug fix TBD.
+  function checkWin(player) {
+    if (bob === 15) {
+      alert("Big win TicTacToe Superstar!");
+      location.reload(true);
+
+  } else if (sally === 15) {
+      alert("Big win TicTacToe Superstar!");
+      location.reload(true);
+
+  } else if (turn === 9) {
+      alert('Tie game! If you wish to play again, reset.');
+      location.reload(true);
     }
-  turn++;
-  });
+};
+});
 });
